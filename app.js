eb0af153cffc7ed04180ec4c0ca4457a85e625c3
Dip Kumar Das <dipkumardasdh@gmail.com>
2019-10-28 20:12:23 +0530

    final commit

--- app.js
+++ app.js
@@ -21,36 +21,42 @@ function convertToWord(letter) {
 }
 
 function win(user_c, computer_c) {
+	const smallUserWord = 'USER'.fontsize(3).sub();
+	const smallCompWord = 'COMP'.fontsize(3).sub();
+	const userChoice_div = document.getElementById(user_c);
 	userScore++;
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
-	const smallUserWord = 'USER'.fontsize(3).sub();
-	const smallCompWord = 'COMP'.fontsize(3).sub();
-	// ES6
 	result_p.innerHTML = `${convertToWord(user_c)}${smallUserWord} beats ${convertToWord(
 		computer_c
 	)}${smallCompWord} . You win!`;
+	userChoice_div.classList.add('green-glow');
+	setTimeout(() => userChoice_div.classList.remove('green-glow'), 300);
 }
 
 function lose(user_c, computer_c) {
+	const smallUserWord = 'USER'.fontsize(3).sub();
+	const smallCompWord = 'COMP'.fontsize(3).sub();
+	const userChoice_div = document.getElementById(user_c);
 	computerScore++;
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
-	const smallUserWord = 'USER'.fontsize(3).sub();
-	const smallCompWord = 'COMP'.fontsize(3).sub();
-	// ES6
 	result_p.innerHTML = `${convertToWord(computer_c)}${smallCompWord} beats ${convertToWord(
 		user_c
 	)}${smallUserWord} . You Lost!`;
+	userChoice_div.classList.add('red-glow');
+	setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
 }
 
 function draw(user_c, computer_c) {
 	const smallUserWord = 'USER'.fontsize(3).sub();
 	const smallCompWord = 'COMP'.fontsize(3).sub();
-	// ES6
+	const userChoice_div = document.getElementById(user_c);
 	result_p.innerHTML = `${convertToWord(user_c)}${smallUserWord} equals ${convertToWord(
 		computer_c
 	)}${smallCompWord} . It's a draw!`;
+	userChoice_div.classList.add('gray-glow');
+	setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
 }
 function game(userChoice) {
 	const computerChoice = getComputerChoice();
@@ -74,17 +80,11 @@ function game(userChoice) {
 }
 
 function main() {
-	rock_div.addEventListener('click', function() {
-		game('r');
-	});
+	rock_div.addEventListener('click', () => game('r'));
 
-	paper_div.addEventListener('click', function() {
-		game('p');
-	});
+	paper_div.addEventListener('click', () => game('p'));
 
-	scissors_div.addEventListener('click', function() {
-		game('s');
-	});
+	scissors_div.addEventListener('click', () => game('s'));
 }
 
 main();
